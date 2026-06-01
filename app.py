9567afa0f975ebf4b61989d947bf171f71535fc7
Madhu <madhumithark7@gmail.com>
2026-06-01 12:04:09 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    universities = []
+
+    if request.method == "POST":
+
+        cgpa = float(request.form["cgpa"])
+        ielts = float(request.form["ielts"])
+        budget = int(request.form["budget"])
+
+        df = pd.read_csv("universities.csv")
+
+        result = df[
+            (df["Min_CGPA"] <= cgpa) &
+            (df["Min_IELTS"] <= ielts) &
+            (df["Max_Budget"] <= budget)
+        ]
+
+        universities = result.to_dict("records")
+
+    return render_template(
+        "index.html",
+        universities=universities
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
